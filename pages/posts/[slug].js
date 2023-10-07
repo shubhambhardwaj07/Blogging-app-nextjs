23a5cd6b70549a9263092485a2c42bf36817e552
shubh <shubham998845@gmail.com>
2023-10-07 14:37:40 +0530

    initial setup of blog app

---
+++ pages/posts/[slug].js
@@ -0,0 +1,7 @@
+export default function PostDetailPage() {
+  return (
+    <div>
+      <h1>Posts details Page</h1>
+    </div>
+  );
+}
---
+++ pages/posts/index.js
@@ -0,0 +1,7 @@
+export default function AllPostsPage() {
+  return (
+    <div>
+      <h1>All Posts Page</h1>
+    </div>
+  );
+}
